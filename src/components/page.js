022e9ed1f00d92247c8ac59e76ecbc3deecc6458
Shaharbsh <shaBSH23>
2021-03-10 21:44:50 +0200

    commit

--- src/components/page.js
+++ src/components/page.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Pokemon from '../components/pokemon'
 import Search from '../components/search'
-
+//commit
 class Page extends Component {
   componentDidMount() {
     this.props.getPokemons()
